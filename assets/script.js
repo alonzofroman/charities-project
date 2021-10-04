4e2d3e9d52852bb0bbf5d49918a9e978d7886d7e
Matt <matthiasvstephens@gmail.com>
2021-10-04 18:55:11 +0200

    Basic index.html layout

--- assets/script.js
+++ assets/script.js
@@ -7,4 +7,22 @@ API KEY: 47ee0338250fd0f2fde645b300727ded/
 Great tutorial: https://www.youtube.com/watch?v=Zxf1mnP5zcw&ab_channel=TraversyMedia
 Docs :https://developers.google.com/maps/documentation/javascript/overview?hl=en_US
 
-API KEY: AIzaSyBGyCeq_y1j0ceJhDdpK7A8DDU-0wu-uSU*/
+API KEY: AIzaSyBGyCeq_y1j0ceJhDdpK7A8DDU-0wu-uSU */
+
+const splashDiv = $('#splashDiv');
+const mainDiv = $('#mainDiv');
+
+/*flickity for carousel*/
+$('#featuredCarousel').flickity({
+  autoPlay: true,
+  wrapAround: true,
+  cellAlign: 'left',
+  contain: true
+});
+
+function loadMain(){
+  splashDiv.hide();
+  mainDiv.show();
+}
+
+$('#initializeBtn').on('click', loadMain);
